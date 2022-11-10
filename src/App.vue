58ab503b952647f45d83ec52ea8fe751d01bf6db
matteoga95 <gallo.work95@gmail.com>
2022-11-10 17:22:37 +0100

    header+content

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,26 @@
-<script setup>
+<script >
+import headerSite from "./components/header.vue"
 
+export default {
+  components: {
+    headerSite
+  }
+}
 </script>
 
 <template>
+  <headerSite />
+  <div id="content">
+    <div class="container">
+      <h2 class="text-light p-5">--> Content goes Here &lt--</h2>
+    </div>
 
+  </div>
 </template>
 
-<style scoped>
+<style >
+#content {
+  background-color: black;
 
+}
 </style>
---
+++ src/components/header.vue
@@ -0,0 +1,65 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+
+    <nav class="navbar navbar-expand-lg p-5">
+        <div class="container-fluid d-flex justify-content-between">
+            <img src="../assets/img/dc-logo.png" alt="">
+
+            <div id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item fw-bold px-2 pb-3">
+                        <a class="nav-link active" href="#">CHARACTERS</a>
+                    </li>
+                    <li class="nav-item fw-bold px-2 pb-3">
+                        <a class="nav-link active" href="#">COMICS</a>
+                    </li>
+                    <li class="nav-item fw-bold px-2 pb-3">
+                        <a class="nav-link active" href="#">MOVIES</a>
+                    </li>
+                    <li class="nav-item fw-bold px-2 pb-3">
+                        <a class="nav-link active" href="#">TV</a>
+                    </li>
+                    <li class="nav-item fw-bold px-2 pb-3">
+                        <a class="nav-link active" href="#">GAMES</a>
+                    </li>
+                    <li class="nav-item fw-bold px-2 pb-3">
+                        <a class="nav-link active" href="#">COLLECTIBLES</a>
+                    </li>
+                    <li class="nav-item fw-bold px-2 pb-3">
+                        <a class="nav-link active" href="#">VIDEOS</a>
+                    </li>
+                    <li class="nav-item fw-bold px-2 pb-3">
+                        <a class="nav-link active" href="#">FANS</a>
+                    </li>
+                    <li class="nav-item fw-bold px-2 pb-3">
+                        <a class="nav-link active" href="#">NEWS</a>
+                    </li>
+                    <li class="nav-item fw-bold px-2 pb-3">
+                        <a class="nav-link active" href="#">SHOP</a>
+                    </li>
+
+
+
+
+
+                </ul>
+
+            </div>
+        </div>
+    </nav>
+
+</template>
+
+
+<style>
+.nav-item:hover {
+    border-bottom: 0.5rem solid;
+    border-color: #0282f9;
+
+}
+</style>
